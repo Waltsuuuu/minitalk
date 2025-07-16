3ad58114723701ff671218b938e5e3cc557ea7de
Waltteri Heino <waltteriheino@gmail.com>
2025-07-16 14:54:36 +0300

    Added 100 microsecond delay before server sends signal acknwledgement to client. Seemed to fix the bug where server was a signal to client, before client was ready to recieve it

--- src/server/server.c
+++ src/server/server.c
@@ -6,7 +6,7 @@
 /*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:06:46 by wheino            #+#    #+#             */
-/*   Updated: 2025/07/16 14:01:26 by wheino           ###   ########.fr       */
+/*   Updated: 2025/07/16 14:51:58 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	handle_signal(int sig, siginfo_t *info, void *context)
 		bit_index = 0;
 		current_char = 0;
 	}
+	usleep(100);
 	kill(info->si_pid, SIGUSR1);
 }
 
