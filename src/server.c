4569285dc0b462cf91e0c96fb6c2e6137a8f7496
Waltteri Heino <waltteriheino@gmail.com>
2025-07-16 12:41:21 +0300

    Set up sigusr1/sigusr2 signal handling with sigaction. Added handle_signal function to reconstruct character bit by bit from the recieved signals

--- src/server.c
+++ src/server.c
@@ -6,20 +6,44 @@
 /*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:06:46 by wheino            #+#    #+#             */
-/*   Updated: 2025/07/15 19:41:20 by wheino           ###   ########.fr       */
+/*   Updated: 2025/07/15 21:15:20 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	handle_signal(int sig, siginfo_t *info, void *context)
+{
+	static int	bit_index = 0;
+	static int	current_char = 0;
+
+	(void)context;
+	if (sig == SIGUSR2)
+		current_char = current_char | (1 << (7 - bit_index));
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == '\0')
+			write(1, "\n", 1);
+		else
+			write(1, &current_char, 1);
+		bit_index = 0;
+		current_char = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
+}
+
 int	main(void)
 {
-	pid_t	pid;
-	
-	pid = getpid();
-	ft_printf("Server PID: %d\n", pid);
-	
-	while(1)
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("Server PID: %d\n", getpid());
+	while (1)
 		pause();
 	return (EXIT_SUCCESS);
 }
