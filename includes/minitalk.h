460bfebb01677702bc610806e22bbf404bc21f79
Waltteri Heino <waltteriheino@gmail.com>
2025-07-16 12:42:14 +0300

    Norminette changes to minitalk.h

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,18 +6,18 @@
 /*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:06:43 by wheino            #+#    #+#             */
-/*   Updated: 2025/07/15 19:39:54 by wheino           ###   ########.fr       */
+/*   Updated: 2025/07/15 21:14:36 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <unistd.h> // write, pause, usleep, sleep
-#include <signal.h> // signal, sigemptyset, sigaddset, sigaction, kill
-#include <stdlib.h> // malloc, free, exit
-#include <sys/types.h> //getpid
-#include "libft.h"
-#include "ft_printf.h"
+# include <unistd.h> // write, pause, usleep, sleep
+# include <signal.h> // signal, sigemptyset, sigaddset, sigaction, kill
+# include <stdlib.h> // malloc, free, exit
+# include <sys/types.h> //getpid
+# include "libft.h"
+# include "ft_printf.h"
 
 #endif
