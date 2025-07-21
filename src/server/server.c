940470d6eb44a3cb1a61ab289eaa65bc977165d8
Waltteri Heino <waltteriheino@gmail.com>
2025-07-21 19:03:27 +0300

    server checks if it starts recieving signals from other pid while printing message from first pid. if it does, it sends sigusr2 to the client

--- src/server/server.c
+++ src/server/server.c
@@ -6,31 +6,49 @@
 /*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 15:06:46 by wheino            #+#    #+#             */
-/*   Updated: 2025/07/17 14:11:38 by wheino           ###   ########.fr       */
+/*   Updated: 2025/07/21 17:42:36 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	*print_byte(char current_char, pid_t *current_client)
+{
+	if (current_char == '\0')
+	{
+		write(1, "\n", 1);
+		*current_client = 0;
+	}
+	else
+		write(1, &current_char, 1);
+	return (current_client);
+}
+
 void	handle_signal(int sig, siginfo_t *info, void *context)
 {
-	static int	bit_index = 0;
-	static int	current_char = 0;
+	static int		bit_index = 0;
+	static int		current_char = 0;
+	static pid_t	current_client;
+	pid_t			client;
 
 	(void)context;
+	client = info->si_pid;
+	if (current_client == 0)
+		current_client = client;
+	if (client != current_client)
+	{
+		kill(client, SIGUSR2);	
+		return ;
+	}
 	if (sig == SIGUSR2)
 		current_char = current_char | (1 << (7 - bit_index));
 	bit_index++;
 	if (bit_index == 8)
 	{
-		if (current_char == '\0')
-			write(1, "\n", 1);
-		else
-			write(1, &current_char, 1);
+		print_byte(current_char, &current_client);
 		bit_index = 0;
 		current_char = 0;
 	}
-	usleep(200);
 	kill(info->si_pid, SIGUSR1);
 }
 
@@ -41,6 +59,8 @@ int	main(void)
 	sa.sa_sigaction = &handle_signal;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("Server PID: %d\n", getpid());
